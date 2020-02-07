3482fbea156ce344ba61f9358052c115ad42db2f
Rubem Silva <rubekx@gmail.com>
2020-02-07 15:34:05 -0300

    Update conexao.js

--- infraestrutura/conexao.js
+++ infraestrutura/conexao.js
@@ -3,8 +3,8 @@ const mysql = require('mysql')
 const conexao = mysql.createConnection({
   host: '10.16.0.223',
   port: 3306,
-  user: 'root',
-  password: 'Nut1s2012',
+  user: '######',
+  password: '#######',
   database: 'sints',
   multipleStatements: true
 })
